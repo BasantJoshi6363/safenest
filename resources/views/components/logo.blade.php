eb1f010016069049edbe747db350c4b84972a4e8
bibekadk2001 <bibekadk2001@gmail.com>
2026-07-25 18:24:09 +0000

    add logo component for branding in the application

---
+++ resources/views/components/logo.blade.php
@@ -0,0 +1,6 @@
+
+<div class="flex-shrink-0">
+                <a href="#" class="text-2xl font-bold text-gray-900 tracking-tight">
+                    Safe<span class="text-indigo-600">Nest.</span>
+                </a>
+            </div>
