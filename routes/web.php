ee3736b7507cecb4716764e95230616fda9517a4
bibekadk2001 <bibekadk2001@gmail.com>
2026-07-25 18:24:35 +0000

    add routes for about, contact, hotels, and rooms pages to enhance navigation

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,24 @@ Route::get('/login',function() {
 Route::get('/register',function() {
     return view('auth.register');
 });
+Route::get('/about',function() {
+    return view('about');
+});
+Route::get('/contact',function() {
+    return view('contact');
+});
+
+Route::get('/hotels', function () {
+    return view('hotels');
+});
+
+Route::get('/rooms', function () {
+    return view('rooms');
+});
+
+Route::get('/',function() {
+    return view('welcome');
+});
 
 
 Route::post('/login');
