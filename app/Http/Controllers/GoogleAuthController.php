5a4b2c955ed5e8f206a812b4ea812759639ae46b
basantjoshi6363 <basantjoshi6363@gmail.com>
2026-07-31 23:23:44 +0545

    add GoogleAuthController for Google authentication support

---
+++ app/Http/Controllers/GoogleAuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleAuthController extends Controller
+{
+        public function redirect()
+    {
+        return Socialite::driver('google')
+            ->with(['prompt' => 'select_account'])
+            ->redirect();
+    }
+
+    public function callback()
+    {
+        // Get user information from Google
+        $googleUser = Socialite::driver('google')->user();
+
+        // Find existing user or create a new user
+        $user = User::updateOrCreate(
+            [
+                'email' => $googleUser->getEmail(),
+            ],
+            [
+                'name' => $googleUser->getName(),
+                'google_id' => $googleUser->getId(),
+                'avatar' => $googleUser->getAvatar(),
+
+                // Google users don't need to know this password
+                'password' => bcrypt(Str::random(32)),
+            ]
+        );
+
+        // Log the user into Laravel
+        Auth::login($user);
+
+        // Redirect to dashboard
+        return redirect('/dashboard');
+    }
+
+        public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+}
+}
