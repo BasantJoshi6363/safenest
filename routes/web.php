51a692918a145a2243648095ac2bff23687601cb
basantjoshi6363 <basantjoshi6363@gmail.com>
2026-07-31 23:25:15 +0545

    refactor route definitions to organize public, guest, and authenticated routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GoogleAuthController;
+use App\Http\Controllers\FacebookLoginController;
 
-Route::get('/login',function() {
-    return view('auth.login');
+/*
+|--------------------------------------------------------------------------
+| Public Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/register',function() {
-    return view('auth.register');
-});
-Route::get('/about',function() {
+Route::get('/about', function () {
     return view('about');
 });
-Route::get('/contact',function() {
+
+Route::get('/contact', function () {
     return view('contact');
 });
 
@@ -24,9 +30,49 @@ Route::get('/rooms', function () {
     return view('rooms');
 });
 
-Route::get('/',function() {
-    return view('welcome');
+/*
+|--------------------------------------------------------------------------
+| Guest Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::middleware('guest')->group(function () {
+
+    Route::get('/login', function () {
+        return view('auth.login');
+    })->name('login');
+
+    Route::get('/register', function () {
+        return view('auth.register');
+    })->name('register');
+
+    // Google Authentication
+    Route::get('/auth/google', [GoogleAuthController::class, 'redirect'])
+        ->name('google.redirect');
+
+    Route::get('/auth/google/callback', [GoogleAuthController::class, 'callback'])
+        ->name('google.callback');
+
+    // Facebook Authentication
+    Route::get('/auth/facebook', [FacebookLoginController::class, 'redirect'])
+        ->name('facebook.redirect');
+
+    Route::get('/auth/facebook/callback', [FacebookLoginController::class, 'callback'])
+        ->name('facebook.callback');
 });
 
+/*
+|--------------------------------------------------------------------------
+| Authenticated Routes
+|--------------------------------------------------------------------------
+*/
 
-Route::post('/login');
+Route::middleware('auth')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('welcome');
+    })->name('dashboard');
+
+    Route::post('/logout', [GoogleAuthController::class, 'logout'])
+        ->name('logout');
+});
