3f51255a0a8904cc5483e800db11d44772bd2ff8
basantjoshi6363 <basantjoshi6363@gmail.com>
2026-08-20 15:17:02 +0545

    Add favicon links to settings page for improved branding

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -4,6 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Account Settings - SafeNest</title>
+
+    <link rel="icon" type="image/png" href="/images/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/images/favicon/favicon.svg" />
+    <link rel="shortcut icon" href="/images/favicon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
+    <link rel="manifest" href="/images/favicon/site.webmanifest" />
+
     @vite('resources/css/app.css')
 </head>
 <body class="bg-gray-50 font-sans antialiased">
