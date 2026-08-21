5017cc0e79e41228ac68042fc83d133e163c1b59
basantjoshi6363 <basantjoshi6363@gmail.com>
2026-08-21 11:35:43 +0545

    Remove favicon links from profile page for cleaner code

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,15 +6,10 @@
     <title>My Profile - SafeNest</title>
     @vite('resources/css/app.css')
 
-    <link rel="icon" type="image/png" href="/images/favicon/favicon-96x96.png" sizes="96x96" />
-    <link rel="icon" type="image/svg+xml" href="/images/favicon/favicon.svg" />
-    <link rel="shortcut icon" href="/images/favicon/favicon.ico" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
-    <link rel="manifest" href="/images/favicon/site.webmanifest" />
-
+    
 </head>
 <body class="bg-gray-50 font-sans antialiased">
-
+ <x-navbar/>
     <div class="min-h-screen flex flex-col md:flex-row">
         
         <!-- Left Side: Dynamic Profile Showcase -->
