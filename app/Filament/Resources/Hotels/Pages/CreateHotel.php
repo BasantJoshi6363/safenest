c4e1b1aa62d4508520c1c69bde8a5af6d68d8fce
basantjoshi6363 <basantjoshi6363@gmail.com>
2026-08-29 22:53:37 +0545

    Send email notification upon hotel creation

--- app/Filament/Resources/Hotels/Pages/CreateHotel.php
+++ app/Filament/Resources/Hotels/Pages/CreateHotel.php
@@ -3,9 +3,19 @@
 namespace App\Filament\Resources\Hotels\Pages;
 
 use App\Filament\Resources\Hotels\HotelResource;
+use App\Mail\HotelCreatedMail;
 use Filament\Resources\Pages\CreateRecord;
+use Mail;
 
 class CreateHotel extends CreateRecord
 {
     protected static string $resource = HotelResource::class;
+
+     protected function afterCreate(): void
+    {
+        $hotel = $this->record;
+
+        Mail::to($hotel->email)
+            ->send(new HotelCreatedMail($hotel));
+    }
 }
