1cfb633ba71af727559291d93fd624fa2703a7a4
basantjoshi6363 <basantjoshi6363@gmail.com>
2026-09-01 13:29:12 +0545

    Add CreateHotelRegistrationRequest page for hotel registration requests

---
+++ app/Filament/Resources/HotelRegistrationRequests/Pages/CreateHotelRegistrationRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\HotelRegistrationRequests\Pages;
+
+use App\Filament\Resources\HotelRegistrationRequests\HotelRegistrationRequestResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateHotelRegistrationRequest extends CreateRecord
+{
+    protected static string $resource = HotelRegistrationRequestResource::class;
+}
