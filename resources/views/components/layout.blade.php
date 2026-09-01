cb9c7439c8e2775ea9387c8cbb80a9f7fc374e01
basantjoshi6363 <basantjoshi6363@gmail.com>
2026-09-01 13:57:29 +0545

    Update layout to include JavaScript assets and remove unused Rojit view

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css" rel="stylesheet" />
 
     <!-- Favicons & Manifest using direct absolute paths -->
